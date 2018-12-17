ab97e39813172d14037590c6689f72e7af1c8bb3
Rob Benton <rmbenton@arkbluecross.com>
2018-12-17 15:11:45 -0600

    Fixed logging

--- src/main/java/rob/rest/aspect/SecureAspect.java
+++ src/main/java/rob/rest/aspect/SecureAspect.java
@@ -6,6 +6,9 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import rob.rest.controller.SecureController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * SecureAspect is an aspect class that defines pointcuts for requests sent to
@@ -19,14 +22,37 @@ public class SecureAspect
 {
     private static final Logger log = LoggerFactory.getLogger(SecureAspect.class);
 
-    @Pointcut("within(rob.rest.controller.SecureController) && @annotation(Logging)")
-    private void loggingPointcut()
+    @Pointcut(value = "within(rob.rest.controller.SecureController)")
+    private void secureControllerPointcut()
     {
     }
 
-    @Before("loggingPointcut()")
-    public void logSecureControllerBefore()
+    @Pointcut("secureControllerPointcut() && @annotation(Logging)")
+    private void logPointcut()
     {
-        log.info("Before secure controller.");
+    }
+
+    /**
+     * Logs the executions of any methods in the {@link SecureController}
+     * class that are annotated with {@link Logging} and do not have a
+     * {@link HttpServletRequest} as their first parameter.
+     */
+    @Before("logPointcut() && !args(javax.servlet.http.HttpServletRequest)")
+    public void logSecureController()
+    {
+        log.info("Logging without request object.");
+    }
+
+    /**
+     * Logs the executions of any methods in the {@link SecureController} class
+     * that are annotated with {@link Logging} and have a
+     * {@link HttpServletRequest} as their first parameter.
+     *
+     * @param httpServletRequest HttpServletRequest
+     */
+    @Before("logPointcut() && args(javax.servlet.http.HttpServletRequest) && args(httpServletRequest)")
+    public void logSecureControllerRequest(HttpServletRequest httpServletRequest)
+    {
+        log.info("Servlet request: " + httpServletRequest.toString());
     }
 }
