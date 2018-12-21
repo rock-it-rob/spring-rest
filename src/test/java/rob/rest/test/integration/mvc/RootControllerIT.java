6461fb97f7fe94a29c67dc06444f4c1905aada72
Rob Benton <rmbenton@arkbluecross.com>
2018-12-21 09:04:52 -0600

    Simplified testing

--- src/test/java/rob/rest/test/integration/mvc/RootControllerIT.java
+++ src/test/java/rob/rest/test/integration/mvc/RootControllerIT.java
@@ -1,15 +1,14 @@
-package rob.rest.test.integration;
+package rob.rest.test.integration.mvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -26,8 +25,7 @@ import static org.junit.Assert.*;
  * @author Rob Benton
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = RootController.class)
 public class RootControllerIT
 {
     private static ObjectMapper objectMapper;
